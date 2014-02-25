3c3e702c7de9ff0500ced0116a74beb11b76bf99
kishiamy <kishiamy84@gmail.com>
2014-02-26 00:34:59 +0100

    [chapter_1] wip: Polimorfismo

--- Complejos/src/Principal.java
+++ Complejos/src/Principal.java
@@ -1,9 +1,17 @@
+
 class Principal {
     public static void main(String[] main) {
-        ComplejoAmpliado z, w;
-        z = new ComplejoAmpliado(-1.5, 0);
-        w = new ComplejoAmpliado(1.7, 0);
-        z.suma(w.real, w.imaginario);
-        System.out.println("Complejo: " + z);
+    	Object lista[] = new Object[4];
+    	lista[0] = new Complejo(-1.5, 3.0);
+    	lista[1] = new GeoPunto(3.5, 7.2);
+    	lista[2] = new GeoPuntoAlt(1.2, 5).setAltura(3E-2);
+    	lista[3] = new GeoPuntoAlt(2.7, 3.2).setAltura(2E7);
+    	
+    	for(int i = 0; i < lista.length; i++) {
+    		  System.out.println("Objeto " + i + ": " + lista[i]);
+    		  if(lista[i] instanceof GeoPuntoAlt && ((GeoPuntoAlt)lista[i]).getAltura() > 1E6) {
+    		    System.out.println("punto elevado!");
+    		  }
+    		}
     }
 }
