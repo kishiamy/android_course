6c3490c0aed014fd85ec68da9c9c7a67b0910671
kishiamy <kishiamy84@gmail.com>
2014-02-26 20:51:27 +0100

    wip

--- Complejos/src/Principal.java
+++ Complejos/src/Principal.java
@@ -2,10 +2,15 @@
 class Principal {
     public static void main(String[] main) {
     	Object lista[] = new Object[4];
+    	GeoPuntoAlt g = new GeoPuntoAlt(1.2, 5);
+    	g.setAltura(3E7);
+    	GeoPuntoAlt h = new GeoPuntoAlt(2.7, 3.2);
+    	h.setAltura(1E-8);
+    	
     	lista[0] = new Complejo(-1.5, 3.0);
     	lista[1] = new GeoPunto(3.5, 7.2);
-    	lista[2] = new GeoPuntoAlt(1.2, 5).setAltura(3E-2);
-    	lista[3] = new GeoPuntoAlt(2.7, 3.2).setAltura(2E7);
+    	lista[2] = g;
+    	lista[3] = h;
     	
     	for(int i = 0; i < lista.length; i++) {
     		  System.out.println("Objeto " + i + ": " + lista[i]);
