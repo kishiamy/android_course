63b348c8972df170c4effb4be8d87eb0c3fd2f4c
kishiamy <kishiamy84@gmail.com>
2014-03-03 00:44:22 +0100

    Lugares class

--- Colecciones/src/Principal.java
+++ Colecciones/src/Principal.java
@@ -1,29 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-
+import org.example.mislugares.Lugar;
 public class Principal {
 
 	public static void main(String[] main) {
-		Set<String> conjunto = new TreeSet <String>();
-		
-		conjunto.add("manzana");
-		conjunto.add("pera");
-		conjunto.add("fresa");
-		conjunto.add("naranja");
-		conjunto.remove("pera");
-		for(String s : conjunto) {
-		     System.out.println(s);
-		}
-		
-		List<Complejo> lista = new ArrayList<Complejo>(); 
-		
-		lista.add(new Complejo(1.0, 5.0));
-		lista.add(new Complejo(2.0, 4.2));
-		lista.add(1, new Complejo(3.0, 0.0) );lista.remove(0); 
-		for(Complejo c: lista) {
-		    System.out.println(c);
-		}
+		   for(Lugar l : Lugares.vectorLugares) {
+	             System.out.println(l);
+	       }
 	}
 }
