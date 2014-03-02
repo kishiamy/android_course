478d8ce3e69e6a5ad42c505c4e488deaa71f8f65
kishiamy <kishiamy84@gmail.com>
2014-03-03 00:21:07 +0100

    include class complejo

---
+++ Colecciones/src/Complejo.java
@@ -0,0 +1,47 @@
+class Complejo {
+
+   //declaración de atributos
+  protected double real, imaginario;
+ 
+
+  //declaración de constructor
+  public Complejo(double real, double imaginario) {
+    this.real= real;
+    this.imaginario= imaginario;
+  }
+
+  public double getReal() {
+	  return real;
+  }
+	   
+  public void setReal(double real){
+	  this. real = real;
+  }
+	   
+  public double getImaginario(){
+	  return imaginario;
+  }
+	   
+	  public void setImaginario(double imaginario) {
+	  this.imaginario = imaginario;
+	  }
+  
+  //declaración de métodos
+ public String toString() {
+	 return real + "+" + imaginario + "i";
+  }
+ 
+ public void suma(Complejo v) {
+	 real = real + v.real;
+	 imaginario = imaginario + v.imaginario;
+ }
+ 
+ public void suma(double re, double im) {
+	  real = real + re;
+	  imaginario = imaginario + im;
+	}
+	 
+	public void suma(double re) {
+	  real = real + re;
+	}
+}
--- Colecciones/src/Principal.java
+++ Colecciones/src/Principal.java
@@ -1,6 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-
 
 public class Principal {
 
@@ -15,6 +16,14 @@ public class Principal {
 		for(String s : conjunto) {
 		     System.out.println(s);
 		}
+		
+		List<Complejo> lista = new ArrayList<Complejo>(); 
+		
+		lista.add(new Complejo(1.0, 5.0));
+		lista.add(new Complejo(2.0, 4.2));
+		lista.add(1, new Complejo(3.0, 0.0) );lista.remove(0); 
+		for(Complejo c: lista) {
+		    System.out.println(c);
+		}
 	}
-
 }
