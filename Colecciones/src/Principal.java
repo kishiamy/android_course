60a5daeec38075c82c309e26c6c31149b30c5c35
kishiamy <kishiamy84@gmail.com>
2014-03-03 01:22:01 +0100

    add class lugares

--- Colecciones/src/Principal.java
+++ Colecciones/src/Principal.java
@@ -1,9 +1,29 @@
-import org.example.mislugares.Lugar;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Principal {
 
 	public static void main(String[] main) {
-		   for(Lugar l : Lugares.vectorLugares) {
-	             System.out.println(l);
-	       }
+		Set<String> conjunto = new TreeSet <String>();
+
+		conjunto.add("manzana");
+		conjunto.add("pera");
+		conjunto.add("fresa");
+		conjunto.add("naranja");
+		conjunto.remove("pera");
+		for(String s : conjunto) {
+		     System.out.println(s);
+		}
+
+		List<Complejo> lista = new ArrayList<Complejo>(); 
+
+		lista.add(new Complejo(1.0, 5.0));
+		lista.add(new Complejo(2.0, 4.2));
+		lista.add(1, new Complejo(3.0, 0.0) );lista.remove(0); 
+		for(Complejo c: lista) {
+		    System.out.println(c);
+		}
 	}
 }
--- MisLugaresJava/src/Principal.java
+++ MisLugaresJava/src/Principal.java
@@ -1,15 +1,9 @@
-import org.example.mislugares.TipoLugar;
 import org.example.mislugares.Lugar;
 
 class Principal {
     public static void main(String[] main) {
-        Lugar z;
-       // TipoLugar tipo = tipo.EDUCACION;
-        z = new Lugar("Escuela Politécnica Superior de Gandía",
-                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38.995656,
-                TipoLugar.EDUCACION, 962849300, "http://www.epsg.upv.es",
-                "Uno de los mejores lugares para formarse.", 3);
-        
-        System.out.println("Lugar: " + z.toString());
+    	  for(Lugar l : Lugares.vectorLugares) {
+              System.out.println(l);
+        }
     }
 }
